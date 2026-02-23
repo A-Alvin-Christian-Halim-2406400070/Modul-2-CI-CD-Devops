0513e41ca34cec9fe62c366e71d96c9103bb8e9a
alvinchristian <alvch6@gmail.com>
2026-02-23 20:29:46 +0700

    introduce failing test

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/ProductTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/ProductTest.java
@@ -1,17 +1,18 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeEach;
-
-import static org.junit.jupiter.api.Assertions.*;
 import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 class ProductTest {
     Product product;
     @BeforeEach
     void setUp() {
         this.product = new Product();
         this.product.setProductId(UUID.fromString("eb558e9f-1c39-460e-8860-71af6af63bd6"));
-        this.product.setProductName("Sampo Cap Bambang");
+        this.product.setProductName("Sampo Cap Udil");
         this.product.setProductQuantity(100);
     }
 
