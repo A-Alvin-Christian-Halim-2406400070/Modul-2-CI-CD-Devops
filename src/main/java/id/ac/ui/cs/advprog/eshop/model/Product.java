0d468f9864d8af39b33c08c0d46a1a5d7ddc51a3
alvinchristian <alvch6@gmail.com>
2026-03-03 10:43:28 +0700

    implement SRP

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -1,12 +1,12 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import java.util.UUID;
+
 import lombok.Getter;
 import lombok.Setter;
-import java.util.UUID;
 
 @Getter @Setter
 public class Product {
-
     private UUID productId = UUID.randomUUID();
     private String productName;
     private int productQuantity;
